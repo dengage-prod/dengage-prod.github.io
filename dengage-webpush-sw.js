7cef32ef264da1e4e2000bd2a3dd530049a5e93c
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-04-12 18:37:06 +0300

    feat: tagging added

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,1 +1,8 @@
-importScripts("https://p.dengage.com/p/push/28/b846d121-846e-74c4-6f44-be4ccbc77931/dengage_sw.js");
+
+var swUrl = new URL(location);
+var accountId = swUrl.searchParams.get('account_id') || '';
+var appGuid = swUrl.searchParams.get('app_guid') || '';
+if (accountId && appGuid) {
+    importScripts("https://pcdn.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+}
+        
